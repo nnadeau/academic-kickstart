246b6c1b44247ed4eabcb57d2e757bf8de4c16cb
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-11-06 08:37:19 -0500

    chore: direct download of unsplash images

--- scripts/featured_image.py
+++ scripts/featured_image.py
@@ -1,35 +1,42 @@
 import logging
-import subprocess
 from pathlib import Path
 from typing import Optional
 
 import fire
+import requests
 
 ROOT_DIR = Path(__file__).parents[1]
 CONTENT_DIR = ROOT_DIR / "content"
+BASE_URL = "https://unsplash.com/photos"
 
 
 def get_latest_post_dir():
     md_paths = list(CONTENT_DIR.rglob("index.md"))
     md_paths = sorted(md_paths, key=lambda x: x.stat().st_ctime, reverse=True)
-    latest = md_paths[0].parent
-    logging.info(f"Latest dir: {latest}")
-    return latest
+    latest_dir = md_paths[0].parent
+    logging.info(f"Latest dir: {latest_dir}")
+    return latest_dir
 
 
-def create_image(query: Optional[str] = None, dir_path: Optional[str] = None):
-    if not query:
-        query = input("Query: ")
+def create_image(image_id: Optional[str] = None, dir_path: Optional[str] = None):
+    # get image ID
+    if not image_id:
+        image_id = input("Image ID: ")
+    image_id = image_id.strip()
 
+    # set output dir and path
     if dir_path:
         dir_path = Path(dir_path)
     else:
         dir_path = get_latest_post_dir()
+    output_path = dir_path / "featured.jpg"
 
-    path = dir_path / "featured.jpg"
-    cmd = ["splash", "--save", str(path.resolve()), "--quiet", "--query", query]
-    logging.info(f"Splash command: {' '.join(cmd)}")
-    subprocess.run(cmd)
+    # fetch image
+    url = f"{BASE_URL}/{image_id}/download"
+    r = requests.get(url)
+    logging.info(f"Request: {r}")
+    with open(output_path, "wb") as f:
+        f.write(r.content)
 
 
 if __name__ == "__main__":
